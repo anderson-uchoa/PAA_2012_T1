5ffd124dc2fd00d4e653e0667c9d5e5a23ceea67
Igor Vasconcelos <igorov.se@gmail.com>
2012-10-07 11:28:22 -0300

    Correções na versão dos métodos devido aos problemas com o versionamento

--- PAA_2012_T1/src/questao01/pph/PPH_05.java
+++ PAA_2012_T1/src/questao01/pph/PPH_05.java
@@ -53,11 +53,11 @@ public class PPH_05 {
       // b0 não entram) * 2(porque é a mesma quantidade para o A e para o
       // B).
       int quantityOfInputValues = scanner.nextInt() + 1;
-      //int quantityOfInputValues = 100;
+      // int quantityOfInputValues = 100;
 
       // A razão que deve ser calculada e apresentada no final.
       float finalRatio = 0;
-
+      int iterations = 0;
       // Esta linha é apenas para forçar uma quebra de linha depois dos
       // números.
       scanner.nextLine();
@@ -66,25 +66,23 @@ public class PPH_05 {
       List<OrderedPair> listOriginalPair = Utils.getValuesFromInputFile(scanner, quantityOfInputValues);
 
       long startTime = System.currentTimeMillis();
-      long iterations = 0;
+
       Log.printOntoScreen("Calculando...");
-      while (System.currentTimeMillis() - startTime < 5000) {
-        listOrderedPairs = new LinkedList<OrderedPair>();
-        // Obtém os valores que correspondem ao b = {1,.., n}
-        listOrderedPairs.addAll(listOriginalPair);
-        // Inicia a Lista S com o tamanho de elementos de pares ordenados
-        // e 2 colunas.
-
-        OrderedPair parInicial = new OrderedPair(listOrderedPairs.get(0).getA(), listOrderedPairs.get(0).getB());
-        // Removendo da lista o par inicial
-        listOrderedPairs.remove(0);
-        listS = new LinkedList<OrderedPair>();
-        //Ordanando a lista
-        Utils.merge(0, listOrderedPairs.size(), listOrderedPairs);
-        finalRatio = maximumRation(listOrderedPairs, listS, parInicial);
-        iterations++;
-
-      }
+      // while (System.currentTimeMillis() - startTime < 5000) {
+      listOrderedPairs = new LinkedList<OrderedPair>();
+      // Obtém os valores que correspondem ao b = {1,.., n}
+      listOrderedPairs.addAll(listOriginalPair);
+      // Atribuindo o par inicial
+      OrderedPair parInicial = listOrderedPairs.get(0);
+      // Removendo da lista o par inicial
+      listOrderedPairs.remove(0);
+      listS = new LinkedList<OrderedPair>();
+      // Ordanando a lista
+      utilidade.Mergesort merge = new utilidade.Mergesort();
+      merge.sort(listOrderedPairs);
+      finalRatio = maximumRation(listOrderedPairs, listS, parInicial);
+      iterations++;
+      // }
       long finishTime = System.currentTimeMillis() - startTime;
 
       float media = (float) finishTime / iterations;
@@ -93,7 +91,7 @@ public class PPH_05 {
       Log.printOntoScreen("Conjunto S*: ");
       Log.printList(listS);
 
-      Log.printOntoScreen("Interaçoes realizadas: " + iterations);
+      Log.printOntoScreen("Iteraçoes realizadas: " + iterations);
       Log.printOntoScreenF("Tempo de execução: %f\n", media);
 
     }
@@ -104,27 +102,19 @@ public class PPH_05 {
 
   private float maximumRation(List<OrderedPair> listOrderedPairs, List<OrderedPair> listS, OrderedPair inicialPar) {
     float aux = 0;
-
     this.somaA = inicialPar.getA();
     this.somaB = inicialPar.getB();
-    //int indexS = 0;
-    float R = (float) inicialPar.getA() / inicialPar.getB();
-    //System.out.println("Razão AoB0: " +  R);
-    for (int i = 0; i < listOrderedPairs.size(); i++) {
-      aux = (float) listOrderedPairs.get(i).getA() / listOrderedPairs.get(i).getB();
-      //System.out.println(" aux: " +  aux+ " = " +   arrayOrderedPairs[i][0] + " / " +  arrayOrderedPairs[i][1]);
-      if (aux > R) {
+    float maximumRation = inicialPar.getRatio();
+    for (int i = 0; i <= listOrderedPairs.size() - 1; i++) {
+      aux = listOrderedPairs.get(i).getRatio();
+      if (aux > maximumRation) {
         this.somaA += listOrderedPairs.get(i).getA();
         this.somaB += listOrderedPairs.get(i).getB();
-        R = (float) this.somaA / this.somaB;
+        maximumRation = (float) this.somaA / this.somaB;
         listS.add(listOrderedPairs.get(i));
       }
-      else
-        break;
-      //System.out.println(" aux: " +  aux+ " R: " +  R);
-      // if
-    } // for i
-    return R;
+    }
+    return maximumRation;
   }
 
   private void bubbleSort(List<OrderedPair> list) {
