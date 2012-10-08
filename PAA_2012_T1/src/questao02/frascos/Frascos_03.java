19c89023b90f6a9785b5148edccf635dd288e9ad
Luciano Sampaio <lsampaioweb@gmail.com>
2012-10-08 11:37:31 -0300

    Fazendo as adaptações na questão dos frascos.

--- PAA_2012_T1/src/questao02/frascos/Frascos_03.java
+++ PAA_2012_T1/src/questao02/frascos/Frascos_03.java
@@ -19,9 +19,12 @@ public class Frascos_03 {
   /**
    * O nome do input padrão(usado para testes).
    */
-  // private static final String DEFAULT_INPUT_FILE_NAME =
-  // "src/questao02/frascos/bignum_32_01.txt";
-  private static final String DEFAULT_INPUT_FILE_NAME = "src/questao02/frascos/input_teste.txt";
+  private static final String DEFAULT_INPUT_FILE_NAME = "src/questao02/frascos/bignum_08_01.txt";
+
+  /**
+   * A quantidade de iterações que foi necessária para que o resultado esperado fosse encontrado.
+   */
+  private long                iterations;
 
   public static void main(String[] args) {
     String inputFile;
@@ -59,28 +62,133 @@ public class Frascos_03 {
       // Obtém a quantidade de números contidos neste arquivo.
       int quantityOfInputValues = scanner.nextInt();
 
+      // A quantidade de frascos que vai ser usada em cada teste.
+      int[] quantityOfFlasks = { 1, 2, 4, 8, 16 };
+
       // Esta linha é apenas para forçar uma quebra de linha depois dos
       // números.
       scanner.nextLine();
 
-      int cont = 0;
       String inputValue;
+      long startTime;
+      long finishTime;
+      int cont = 0;
+      // Este loop vai iterar por todos os números encontrados dentro do arquivo de entrada.
       while (cont < quantityOfInputValues) {
         // Obtém o número (como uma string).
-        // Exemplo: 00111110 = 62.
+        // Exemplo: X = 00111110 = 62.
         inputValue = scanner.nextLine();
 
-        // Obtém o valor de N (Tamanho da escada).
-        // int ladderSize = getLadderSize();
+        String stepItBroke = null;
+        // Este loop vai iterar o mesmo número que foi lido do arquivo vezes a quantidade de frascos que vai ser testado. ex: 1, 2, 4, 8, 16
+        for (int flasks : quantityOfFlasks) {
+          startTime = System.currentTimeMillis();
+          iterations = 0;
 
-        Log.debug(inputValue);
+          // Encontra em que degrau da escada o frasco quebrou.
+          stepItBroke = findTheStepItBreaks(inputValue, flasks);
 
+          Log.debugF("Entrada %s, saída %s com %2d frasco(s) precisou de %2d passos. ", inputValue, stepItBroke, flasks, iterations);
+          finishTime = System.currentTimeMillis() - startTime;
+          Log.debugF("Tempo de execução: %d\n", finishTime);
+        }
+
+        Log.debug("");
         cont++;
       }
     }
     catch (FileNotFoundException e) {
       e.printStackTrace();
     }
+  }
+
+  /**
+   * Encontra em que degrau da escada o frasco quebrou.
+   * 
+   * @param inputValue O degrau em que temos certeza que vai quebrar, é um dado usado apenas para comparações.
+   * @param flasks A quantidade de frascos disponivel para este teste.
+   * @return O degrau da escada em que o frasco quebrou.
+   */
+  private String findTheStepItBreaks(String inputValue, int flasks) {
+    // Converte a string de entrada em um array de booleans(true ou false).
+    boolean[] input = convertFromString(inputValue);
+    // Um array que irá contér o degrau máximo em que o frasco é quebrado. 
+    boolean[] output = new boolean[input.length];
+
+    // Baseado na quantidade de frascos disponíveis, precisamos saber de quantos em quantos passos(bits) vamos andar.
+    int eachStep = inputValue.length() / flasks;
+
+    int starPos = 0;
+    // O -1 é para evitar indexOutOfBound, porque um elemento com 8 bits, vai de 0 à 7.
+    int endPos = eachStep - 1;
+
+    // A quantidade de frascos que já usamos.
+    int usedFlasks = 0;
+    while (usedFlasks < flasks) {
+      // Incrementa em 1 o valor do próximo passo.
+      increment(output);
+
+      while (true) {
+        // Aumenta a quantidade de passos(tentativas de quebrar um frasco) que já foram dados.
+        iterations++;
+
+        increment(output);
+        break;
+      }
+
+      // Aumenta em 1 a quantidade de frascos usados.
+      usedFlasks++;
+      starPos = endPos + 1;
+      endPos += eachStep;
+    }
+
+    return convertFromArray(output);
+  }
+
+  /**
+   * @param output
+   */
+  private void increment(boolean[] output) {
+  }
+
+  /**
+   * Converte a string de entrada em um array de booleans(true ou false).
+   * 
+   * @param value O degrau em que temos certeza que vai quebrar, é um dado usado apenas para comparações.
+   * @return Um array de boolean correspondente a string de entrada.
+   */
+  private boolean[] convertFromString(String value) {
+    // Obtém o tamanho da entrada.
+    int length = value.length();
+    boolean[] input = new boolean[length];
+
+    for (int i = 0; i < length; i++) {
+      // Se o valor for "0", então o valor false é adicionado.
+      // Se o valor for "1", então o valor true é adicionado.
+      input[i] = (value.charAt(i) == '0') ? false : true;
+    }
+
+    return input;
+  }
+
+  /**
+   * Converte um array de boolean para uma string, onde true = 1 e false = 0.
+   * 
+   * @param value O array que vai ser convertido
+   * @return Uma string, onde true = 1 e false = 0.
+   */
+  private String convertFromArray(boolean[] value) {
+    StringBuilder output = new StringBuilder();
+
+    // Obtém o tamanho da entrada.
+    int length = value.length;
+    for (int i = 0; i < length; i++) {
+      // Se o valor for true,  então o valor "1" é adicionado.
+      // Se o valor for false, então o valor "0" é adicionado.
+      output.append((value[i]) ? "1" : "0");
+    }
+
+    return output.toString();
   }
 
 }
